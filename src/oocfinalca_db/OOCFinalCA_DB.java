f33265e00940eddb5695cd26b0d9b8764656e62f
C2022188 <2022188@student.cct.ie>
2023-12-18 19:10:00 +0000

    Update OOCFinalCA_DB.java
    Add link GitHUb

--- src/oocfinalca_db/OOCFinalCA_DB.java
+++ src/oocfinalca_db/OOCFinalCA_DB.java
@@ -3,18 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package oocfinalca_db;
-
 /**
  *
  * @author caroo
  */
+
+
+//GitHub link: https://github.com/C2022188/OOCFinalCA_DB.git
+//Member 1: Carlos Daniel Amnbrosio 2022171
+// Member 2: Caroline Ferreira 2022188
+
+
 public class OOCFinalCA_DB {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
