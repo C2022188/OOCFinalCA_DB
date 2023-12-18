d7baaadf04c1ae64e86acaec04fab382a6e34b4b
2022171 <146198931+2022171@users.noreply.github.com>
2023-12-18 23:19:36 +0000

    Getters and setters
    inserted getters and setters to the class users and a boolean return whether the admin credentials are true

--- src/oocfinalca_db/Users.java
+++ src/oocfinalca_db/Users.java
@@ -37,4 +37,91 @@ public Users(int ID, String firstName, String lastName, String email, String PPS
         this.username = username;
         this.password = password;
     }
+public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPPSN(String PPSN) {
+        this.PPSN = PPSN;
+    }
+
+    public void setGrossIncome(double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public void setTaxCredit(double taxCredit) {
+        this.taxCredit = taxCredit;
+    }
+
+    public void setTaxOwed(double taxOwed) {
+        this.taxOwed = taxOwed;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPPSN() {
+        return PPSN;
+    }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public double getTaxCredit() {
+        return taxCredit;
+    }
+
+    public double getTaxOwed() {
+        return taxOwed;
+    }
+
+
+
+    public static int getCurrentID() {
+        return currentID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    
+    
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+     public boolean isAdmin() {
+        return getUsername().equals("Admin") && getPassword().equals("CCT");
+    }
 }
