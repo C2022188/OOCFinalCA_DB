66a1c8681303d53ef9709e7a9b37ac50bf428d4d
C2022188 <2022188@student.cct.ie>
2023-12-19 00:24:40 +0000

    Update DatabaseWriter.java

--- src/oocfinalca_db/DatabaseWriter.java
+++ src/oocfinalca_db/DatabaseWriter.java
@@ -4,10 +4,39 @@
  */
 package oocfinalca_db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author caroo
  */
 public class DatabaseWriter extends Database {
+    //// This method adds a new user to the database.
+    public boolean newUser(Users users) throws SQLException {
+        try(
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();  
+        ){
+            // Construct the SQL INSERT statement using placeholders to avoid SQL injection.
+            String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
+                    + "'%d', '%s', '%s', '%s', '%s', '%f', '%f', '%f', '%s', '%s');",
+                    users.getID(), users.getFirstName(), users.getLastName(), users.getEmail(), users.getPPSN(), users.getGrossIncome(), 
+                    users.getTaxCredit(), users.getTaxOwed(), users.getUsername(), users.getPassword());
+            stmt.execute(sql);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+ 
+    public boolean addAllUsers(List<Users> usersList) { // This method is intended to add a list of users to the database
+        return true;
+    }
     
 }
